646ea31f760855d505b066d212300badce7eb691
Manpreet Kaur <manpreetkaur@Manpreets-MacBook-Air.local>
2023-01-27 21:33:18 +0530

    final

--- my-project/config/env/production/database.js
+++ my-project/config/env/production/database.js
@@ -6,14 +6,12 @@ module.exports = ({ env }) => ({
         "DATABASE_HOST",
         "dpg-cf9u8parrk01l41k2hkg-a.singapore-postgres.render.com"
       ),
-      port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "sample_sugy"),
-      user: env("DATABASE_USERNAME", "admin"),
-      password: env("DATABASE_PASSWORD", "MBsQDFVrsU8oWRoQMzbhtr3MJV7hOxyD"),
-      schema: env("DATABASE_SCHEMA", "public"), // Not required
-      ssl: {
-        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", true), // For self-signed certificates
-      },
+      port: env.int("DATABASE_PORT"),
+      database: env("DATABASE_NAME"),
+      user: env("DATABASE_USERNAME"),
+      password: env("DATABASE_PASSWORD"),
+      schema: env("DATABASE_SCHEMA"), // Not required
+      ssl: env.bool("DATABASE_SSL", true),
     },
     debug: false,
   },
