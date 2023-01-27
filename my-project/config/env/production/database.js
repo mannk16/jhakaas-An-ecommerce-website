add5cc14724b465b3386857ee66717079ea49de5
Manpreet Kaur <manpreetkaur@Manpreets-MacBook-Air.local>
2023-01-27 21:27:22 +0530

    final

--- my-project/config/env/production/database.js
+++ my-project/config/env/production/database.js
@@ -2,14 +2,17 @@ module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env("DATABASE_HOST", "127.0.0.1"),
+      host: env(
+        "DATABASE_HOST",
+        "dpg-cf9u8parrk01l41k2hkg-a.singapore-postgres.render.com"
+      ),
       port: env.int("DATABASE_PORT", 5432),
-      database: env("DATABASE_NAME", "strapi"),
-      user: env("DATABASE_USERNAME", "strapi"),
-      password: env("DATABASE_PASSWORD", "strapi"),
+      database: env("DATABASE_NAME", "sample_sugy"),
+      user: env("DATABASE_USERNAME", "admin"),
+      password: env("DATABASE_PASSWORD", "MBsQDFVrsU8oWRoQMzbhtr3MJV7hOxyD"),
       schema: env("DATABASE_SCHEMA", "public"), // Not required
       ssl: {
-        ca: env("DATABASE_CA"),
+        rejectUnauthorized: env.bool("DATABASE_SSL_SELF", true), // For self-signed certificates
       },
     },
     debug: false,
